4a0ef9fb2b68af3ea98329e39c1cb2236a0174b7
bettykroon <betty.kroon@hotmail.com>
2021-12-07 15:33:49 +0100

    fångar bollarna

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ let bollkalle = document.getElementById("bollkalle");
 let bottom = 0;
 let left = 0;
 
+let bollkalleTop = bottom + 100 + "px";
+let bollkalleLeft = bollkalle.style.left;
+
 document.addEventListener("keydown", (evt) => {
     switch (evt.key) {
         case "ArrowLeft":
             if(left > 0){
                 left -= 10;
                 bollkalle.style.left = left + "px";
+                console.log("vänster", bollkalle.style.left);
             }
             break;
         case "ArrowRight":
             if(left < 450){
                 left += 10;
                 bollkalle.style.left = left + "px";
+                console.log("höger", bollkalle.style.left);
             }
             break;
         default:
@@ -31,14 +36,16 @@ function addFootball() {
 
     console.log("Fotboll kommer!");
 
+    bollkalle.style.left = left;
+
     let football = document.createElement("div");
     football.classList = "football";
 
     let footballTop = 0;
-    let footballPosition = Math.round(Math.floor(Math.random() * (100 - 1)/10)*10);
+    let footballPosition = Math.round(Math.floor(Math.random() * (450 - 1)/10)*10);
 
     football.style.top = footballTop + "px";
-    football.style.left = footballPosition + "%";
+    football.style.left = footballPosition + "px";
     football.id = footballId;
 
     let move = setInterval(() => {
@@ -46,13 +53,16 @@ function addFootball() {
         footballTop += 50;
         document.getElementById(footballId).style.top = footballTop + "px";
 
-        console.log(bollkalle.style.top);
-
-        if (footballTop == 450) {
+        console.log(footballTop);
+        console.log(football.style.left);
+ 
+        if (footballTop == 350 && bollkalle.style.left == football.style.left){
+            console.log("Du fångade!!");
+            clearInterval(move);
+            addFootball();
+        }else if (footballTop == 450) {
             console.log("GAME OVER!!!");
             clearInterval(move);
-        } else if (footballTop == bollkalle.style.top){
-            console.log("HEJ");
         }
 
     }, 1000);
