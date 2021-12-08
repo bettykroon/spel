9f2f47ff5ad9553d6633d7a2ff6765efdef228f8
bettykroon <betty.kroon@hotmail.com>
2021-12-08 08:58:47 +0100

    restart and score

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let game = document.getElementById("game");
 let bollkalle = document.getElementById("bollkalle");
 let gameOver = document.getElementById("gameOver");
+let result = document.getElementById("result");
 
 let bottom = 0;
 let left = 0;
-
-let bollkalleTop = bottom + 100 + "px";
-let bollkalleLeft = bollkalle.style.left;
+let points = 0;
 
 document.addEventListener("keydown", (evt) => {
     switch (evt.key) {
@@ -14,14 +13,14 @@ document.addEventListener("keydown", (evt) => {
             if(left > 0){
                 left -= 10;
                 bollkalle.style.left = left + "px";
-                console.log("vänster", bollkalle.style.left);
+                //console.log("vänster", bollkalle.style.left);
             }
             break;
         case "ArrowRight":
             if(left < 450){
                 left += 10;
                 bollkalle.style.left = left + "px";
-                console.log("höger", bollkalle.style.left);
+                //console.log("höger", bollkalle.style.left);
             }
             break;
         default:
@@ -35,7 +34,7 @@ function addFootball() {
 
     footballId ++;
 
-    console.log("Fotboll kommer!");
+    //console.log("Fotboll kommer!");
 
     bollkalle.style.left = left;
 
@@ -54,17 +53,30 @@ function addFootball() {
         footballTop += 50;
         document.getElementById(footballId).style.top = footballTop + "px";
 
-        console.log(footballTop);
-        console.log(football.style.left);
+        //console.log(footballTop);
+        //console.log(football.style.left);
  
         if (footballTop == 350 && bollkalle.style.left == football.style.left){
-            console.log("Du fångade!!");
+            //console.log("Du fångade!!");
+            points++;
+            let score = document.createElement("h1");
+            score.id = "score";
+            score.innerText = "Score: " + points;
+            result.append(score);
+
             clearInterval(move);
             addFootball();
         }else if (footballTop == 450) {
-            console.log("GAME OVER!!!");
-            gameOver.innerText = "GAME OVER!!!";
+            //console.log("GAME OVER!!!");
+            gameOver.innerHTML = "GAME OVER!!!";
             clearInterval(move);
+            let restart = document.createElement("button");
+            restart.id = "restart";
+            restart.innerText = "Restart";
+            result.append(restart);
+            restart.addEventListener("click", function(){
+                location.reload();
+            })
         }
 
     }, 800);
