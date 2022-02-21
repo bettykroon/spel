10b57bc74a18083163e146384f2c0f1fa3f50d05
bettykroon <betty.kroon@hotmail.com>
2022-02-21 15:43:53 +0100

    Ändrat utseendet på spelet

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let gameOver = document.getElementById("gameOver");
 let result = document.getElementById("result");
 let score = document.getElementById("score");
 
+gsap.from("#header", {duration: 3, x: 300, opacity: 0, scale: 0.5});
+
 let bottom = 0;
-let left = 0;
+let left = 220;
 let points = 0;
 
 document.addEventListener("keydown", (evt) => {
@@ -14,14 +16,14 @@ document.addEventListener("keydown", (evt) => {
             if(left > 0){
                 left -= 10;
                 bollkalle.style.left = left + "px";
-                //console.log("vänster", bollkalle.style.left);
+                console.log("vänster", bollkalle.style.left);
             }
             break;
         case "ArrowRight":
-            if(left < 450){
+            if(left < 426){
                 left += 10;
                 bollkalle.style.left = left + "px";
-                //console.log("höger", bollkalle.style.left);
+                console.log("höger", bollkalle.style.left);
             }
             break;
         default:
@@ -40,10 +42,11 @@ function addFootball() {
     bollkalle.style.left = left;
 
     let football = document.createElement("div");
+    football.innerHTML = "<i class='fab fa-bitcoin'></i>";
     football.classList = "football";
 
     let footballTop = 0;
-    let footballPosition = Math.round(Math.floor(Math.random() * (450 - 1)/10)*10);
+    let footballPosition = Math.round(Math.floor(Math.random() * (426 - 1)/10)*10);
 
     football.style.top = footballTop + "px";
     football.style.left = footballPosition + "px";
@@ -51,11 +54,11 @@ function addFootball() {
 
     let move = setInterval(() => {
 
-        footballTop += 50;
+        footballTop += 10;
         document.getElementById(footballId).style.top = footballTop + "px";
 
         //console.log(footballTop);
-        //console.log(football.style.left);
+        console.log(football.style.left);
  
         if (footballTop == 400 && bollkalle.style.left == football.style.left){
             //console.log("Du fångade!!");
@@ -63,7 +66,11 @@ function addFootball() {
             score.innerHTML = "Score: " + points;
             clearInterval(move);
             football.remove();
-            addFootball();
+            if(points < 5){
+                addFootball();
+            } else {
+                fasterFootball();
+            }
         }else if (footballTop == 450) {
             //console.log("GAME OVER!!!");
             gameOver.innerHTML = "GAME OVER!!!";
@@ -80,7 +87,62 @@ function addFootball() {
                 location.reload();
             })
         }
-    }, 800);
+    }, 300);
     game.append(football);
 }
 addFootball();
+
+function fasterFootball() {
+
+    footballId ++;
+
+    //console.log("Fotboll kommer!");
+
+    bollkalle.style.left = left;
+
+    let football = document.createElement("div");
+    football.innerHTML = "<i class='fab fa-bitcoin'></i>";
+    football.classList = "football";
+
+    let footballTop = 0;
+    let footballPosition = Math.round(Math.floor(Math.random() * (426 - 1)/10)*10);
+
+    football.style.top = footballTop + "px";
+    football.style.left = footballPosition + "px";
+    football.id = footballId;
+
+    let move = setInterval(() => {
+
+        footballTop += 10;
+        document.getElementById(footballId).style.top = footballTop + "px";
+
+        //console.log(footballTop);
+        //console.log(football.style.left);
+        //console.log(bollkalle.style.left);
+ 
+        if (footballTop == 400 && bollkalle.style.left == football.style.left){
+            //console.log("Du fångade!!");
+            points++;
+            score.innerHTML = "Score: " + points;
+            clearInterval(move);
+            football.remove();
+            fasterFootball();
+        }else if (footballTop == 450) {
+            //console.log("GAME OVER!!!");
+            gameOver.innerHTML = "GAME OVER!!!";
+            clearInterval(move);
+
+            // Skapar en restart knapp
+            let restart = document.createElement("button");
+            restart.id = "restart";
+            restart.innerText = "Restart";
+            result.append(restart);
+
+            // om man trycker på knappen så laddas sidan om
+            restart.addEventListener("click", function(){
+                location.reload();
+            })
+        }
+    }, 100);
+    game.append(football);
+}
